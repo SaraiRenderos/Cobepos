3a606e8b213cfadf1bc58bb1da9533dcc6fd19f4
SaraiRenderos <estelarenderosguzman@gmail.com>
2019-05-03 21:16:51 -0600

    inicializando la app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PlatosComponent } from './platos/platos.component';
+import { BebidasComponent } from './bebidas/bebidas.component';
+import { PostresComponent } from './postres/postres.component';
+import { ComentariosComponent } from './comentarios/comentarios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PlatosComponent,
+    BebidasComponent,
+    PostresComponent,
+    ComentariosComponent
   ],
   imports: [
     BrowserModule,
