1f0e0e18980ef721aa0f7bf4d1af5ac83fb5094f
SaraiRenderos <estelarenderosguzman@gmail.com>
2019-05-03 22:00:13 -0600

    creación de ofertas en el home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ServicioService } from '../servicio.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent{
 
-  constructor() { }
+  Ofertas = null;
+  constructor(private servicioService: ServicioService) {
+    this.Ofertas = servicioService.getPlatos();
+   }
 
-  ngOnInit() {
-  }
+  
 
 }
